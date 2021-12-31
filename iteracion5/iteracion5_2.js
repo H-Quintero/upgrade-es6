e0dbc420b596490644229dfba82dfaa27301bd28
Hugo Quintero <hugoquinterogarcia@gmail.com>
2021-12-31 13:28:24 +0100

    Iteraciones 5-6

---
+++ iteracion5/iteracion5_2.js
@@ -0,0 +1,9 @@
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90]
+
+let agesPar = ages.filter(function(par) {
+  return par % 2 == 0
+})
+console.log(agesPar)
---
+++ iteracion5/iteracion5_5.js
@@ -0,0 +1,20 @@
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
+
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+]
+
+let streamersOld = streamers.filter(function(old) {
+  if(old.gameMorePlayed == 'League of Legends' && old.age > 38) {
+    old.gameMorePlayed = old.gameMorePlayed.toUpperCase()
+  }
+  return old
+})
+console.log(streamersOld)
